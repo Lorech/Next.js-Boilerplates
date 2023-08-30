50ef02d05e6e2f7a08773575b53eae47b9aca438
Lorech <laurisdedumets@gmail.com>
2023-08-30 17:35:20 +0300

    Add Sass mixin for breakpoints

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['src/styles'],
+    prependData: `@import '_breakpoints';`,
+  },
 };

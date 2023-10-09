31c12772c4561b1d9ac22a424ed8c1a13bb7ed25
Lorech <laurisdedumets@gmail.com>
2023-10-09 14:58:18 +0300

    Remove unused context provider

--- src/context/Localization/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LocalizationProvider } from './localizationProvider';
-export type { LocalizationProviderProps } from './localizationProvider';
--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -2,22 +2,13 @@
 
 import React from 'react';
 
-import {
-  LocalizationProvider,
-  LocalizationProviderProps,
-} from './Localization';
-
 // The global provider may take in props from the root layout to pass down to the individual providers.
 type ProvidersProps = {
   children: React.ReactNode;
-} & LocalizationProviderProps;
+};
 
-function Providers({ locale, defaultLocale, children }: ProvidersProps) {
-  return (
-    <LocalizationProvider locale={locale} defaultLocale={defaultLocale}>
-      {children}
-    </LocalizationProvider>
-  );
+function Providers({ children }: ProvidersProps) {
+  return { children };
 }
 
 export default Providers;
